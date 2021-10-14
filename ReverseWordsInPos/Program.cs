906e3df53ceed86440e1e26a52941693a6c7e421
hxchua00 <hxchua00@gmail.com>
2021-10-14 12:01:46 +0800

    Add project files.

---
+++ ReverseWordsInPos/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseWordsInPos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Sample text right here";
+            string[] arr = text.Split(' ');
+
+            Console.WriteLine($"Text to reverse: \n{text}");
+            Console.WriteLine();
+
+    
+            
+            
+            for (int i = 0; i < arr.Length; i++)
+            {
+                string substr = arr[i];
+                string temp = "";
+                //Console.WriteLine($"arr[i]: {substr}");
+
+                for (int j = substr.Length-1; j >=0; j--)
+                {
+                    temp += substr[j];
+                }
+                arr[i] = temp;
+                Console.Write($"{arr[i]} ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
